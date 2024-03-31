e253b82ad611cc2c67c316c75df1c4f12131d8f4
ankit gupta <ankitgupta17878@gmail.com>
2024-03-31 22:33:49 +0530

    -

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -24,7 +24,7 @@ function CreatePost() {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://ai-image-craft-2.onrender.com//api/v1/post', {
+        const response = await fetch('https://ai-image-craft-2.onrender.com/api/v1/post', {
 
           method: 'POST',
           headers: {
